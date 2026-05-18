d84ca4d64aa6c72cc03998bb291a2bd7e7f83c90
theacc03-prog <theacc03@gmail.com>
2026-05-18 01:11:01 -0300

    Criar reitoria.js

---
+++ reitoria.js
@@ -0,0 +1,21 @@
+window.reitoria = {
+
+    aplicarBonusAcademico: async (uid) => {
+
+        console.log('Bonus aplicado');
+
+    },
+
+    aplicarOnus: async (uid) => {
+
+        console.log('ônus aplicado');
+
+    },
+
+    trancarConta: async (uid) => {
+
+        console.log('conta congelada');
+
+    }
+
+};
